5c0397edafa4ac651a022e4ad2dc1c4966ef3937
broskke <sapiramida123@gmail.com>
2023-02-04 19:26:41 +0600

    new_file

---
+++ git_commands.py
@@ -0,0 +1,12 @@
+git init -создается особая папка в которой хранится наш репрозиторий .git
+git remote add name url - вместо  name url вставляем ссылку на наш ключ добавляет удаленный репрозиторий на сервер
+git pull origin master
+git status - показывает статус файлов
+git add name file - добавит файлы в индекс
+git commit -m "comment"
+git branch - менеджер веток показывает спимсок веток
+gir branch name branch - создание ветки 
+git checkout name branch - переключает на ветку name branch
+git push name branch - отправляет код в гит
+gti reset filename - удаляет файл из индекса
+ 
